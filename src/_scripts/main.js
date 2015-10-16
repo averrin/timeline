30ea2958560a5e94abc7246f8a90e9de2e7d6ff1
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2015-10-16 17:55:38 +0300

    init 2

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,9 +4,21 @@
 'use strict';
 
 import $ from 'jquery';
-import Link from '../_modules/link/link';
 
 $(() => {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+  let queue = new createjs.LoadQueue(true);
+  queue.on('fileload', init, this);
+  queue.loadFile('timeline.json');
 });
+
+function init(event) {
+  let data = event.item;
+  console.log(data);
+  let stage = new createjs.Stage('timeline');
+  let circle = new createjs.Shape();
+  circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, 50);
+  circle.x = 100;
+  circle.y = 100;
+  stage.addChild(circle);
+  stage.update();
+}
