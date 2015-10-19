59817c591e1c5dd0a92b359688f266e9ad67b1fb
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2015-10-19 21:06:20 +0300

    refactor

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,9 +4,10 @@
 'use strict';
 
 import $ from 'jquery';
+// import Container from 'createjs';
+
 
 let stage;
-let dots = [];
 
 const style = {
   dotBG: '#2c5e40',
@@ -19,12 +20,129 @@ const style = {
   mainBG: '#111',
   itemZoom: 1.3,
   itemAlpha: 0.7,
+  textFont: '9pt Sans',
 };
+
+class Range {
+  constructor(parent, year) {
+    this.data = parent.data;
+    this.index = parent.data.years.indexOf(year);
+    this.container = new createjs.Container();
+    this.year = year;
+    let step = style.width / parent.data.years.length;
+    let mstep = step / 12;
+
+    let x = step * this.index;
+    let y = style.height / 2;
+
+    if (this.data.map[this.year]) {
+      for (let [i, r] of this.data.map[this.year].entries()) {
+        let m = parseInt(r.startDate.split('-')[1]);
+        let m2 = parseInt(r.endDate.split('-')[1]);
+        let y2 = parseInt(r.endDate.split('-')[0]);
+        let item = new createjs.Shape();
+        item.graphics.beginFill(r.color).drawRect(0, 0, (y2-this.year)*step + (m2-m)*mstep, (r.studyType ? style.itemHeightBottom : style.itemHeight) + i*style.hStep);
+        item.x = x + (m-1) * mstep;
+        item.y = y - style.itemHeight - 2;
+        if (r.studyType) {
+          item.y = y + 2;
+        }
+        item.scaleX = 0;
+        this.container.addChild(item);
+        let orig_y = item.y;
+
+        let anim = createjs.Tween.get(item).to({scaleX: 1}, 800, createjs.Ease.cubicOut).call(() => {
+          let l = this.addLabel(item, r);
+          item.on('mouseover', (e) => {
+            let animOver = createjs.Tween.get(item);
+            let animOverL = createjs.Tween.get(l);
+            animOverL.to({scaleX: 1.2, scaleY: 1.2}, 100);
+            if (!r.studyType) {
+              animOver.to({alpha: style.itemAlpha, scaleY: style.itemZoom, y: orig_y - style.itemHeight * (style.itemZoom - 1)}, 100);
+            } else {
+              animOver.to({alpha: style.itemAlpha, scaleY: style.itemZoom}, 100);
+            }
+          });
+          item.on('mouseout', (e) => {
+            let animOut = createjs.Tween.get(item);
+            let animOverL = createjs.Tween.get(l);
+            animOverL.to({scaleX: 1, scaleY: 1}, 100);
+            animOut.to({alpha: 1, scaleY: 1, y: orig_y}, 100);
+          });
+        });
+      }
+    }
+  }
+  addLabel(item, r) {
+    let label;
+    let angle = 0;
+    if (!r.studyType) {
+      label = new createjs.Text(r.company, style.textFont, style.textFG);
+      label.x = item.x + 30;
+      label.y = item.y - 20;
+      angle = -45;
+    } else {
+      label = new createjs.Text(r.institution, style.textFont, style.textFG);
+      label.x = item.x + 30;
+      label.y = item.y + style.itemHeightBottom + 16;
+    }
+
+    label.alpha = 0;
+    label.rotation = 0;
+    let anim = createjs.Tween.get(label).to({alpha: 1, rotation: angle}, 400, createjs.Ease.cubicOut);
+    this.container.addChild(label);
+    return label;
+  }
+}
+
+// class TimeMark extends Container {
+class TimeMark {
+  constructor(parent, year) {
+    // super();
+    this.year = year;
+    this.index = parent.data.years.indexOf(year);
+    this.data = parent.data;
+    this.container = new createjs.Container();
+    let step = style.width / parent.data.years.length;
+    let mstep = step / 12;
+    let dot = new createjs.Shape();
+    dot.graphics.beginFill(style.dotBG)
+      .setStrokeStyle(0.5).s(style.textFG).drawCircle(0, 0, 1);
+    dot.x = step * this.index;
+    dot.y = style.height / 2;
+    let anim = createjs.Tween.get(dot)
+      .to({scaleX: 5, scaleY: 5}, 100, createjs.Ease.cubicOut);
+    let label = new createjs.Text(this.year, style.textFont, style.textFG);
+    label.x = dot.x + 8;
+    label.y = dot.y + 4;
+
+    this.container.addChild(dot);
+    this.container.addChild(label);
+    let ha = new createjs.Shape();
+    ha.graphics.beginFill("#000").drawRect(dot.x - 3, dot.y - 3, 20 + label.getMeasuredWidth(), 30);
+    this.container.hitArea = ha;
+    let orig_x = label.x;
+    this.container.on('mouseover', (e) => {
+      let animOver = createjs.Tween.get(dot);
+      animOver.to({scaleX: 7, scaleY: 7}, 100);
+      let animOverL = createjs.Tween.get(label);
+      animOverL.to({rotation: 30, x: orig_x + 5}, 100);
+    });
+    this.container.on('mouseout', (e) => {
+      let animOut = createjs.Tween.get(dot);
+      animOut.to({scaleX: 5, scaleY: 5}, 100);
+      let animOutL = createjs.Tween.get(label);
+      animOutL.to({rotation: 0, x: orig_x}, 100);
+    });
+  }
+}
 
 class Timeline {
   constructor(data) {
     this.data = this.processData(data);
     this.stage = new createjs.Stage('timeline');
+    this.marks = [];
+    this.ranges = [];
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener('tick', this.stage);
     this.render();
@@ -44,16 +162,21 @@ class Timeline {
       anim.to({scaleX: scale}, 200, createjs.Ease.cubicOut)
         .call(() => {
           if (y != 0) {
-            this.addCircle(y, i, step);
+            let mark = new TimeMark(this, y);
+            this.marks.push(mark);
+            let range = new Range(this, y);
+            this.ranges.push(range);
+            this.stage.addChild(range.container);
+            this.stage.addChild(mark.container);
           }
         });
       scale += step;
     }
 
-    let work = new createjs.Text('work', '9pt Sans', style.textFG);
+    let work = new createjs.Text('work', style.textFont, style.textFG);
     work.x = 10;
     work.y = style.height / 2 - 2 - 20;
-    let edu = new createjs.Text('education', '9pt Sans', style.textFG);
+    let edu = new createjs.Text('education', style.textFont, style.textFG);
     edu.x = 10;
     edu.y = style.height / 2 + 2 + 4;
 
@@ -109,100 +232,6 @@ class Timeline {
     return data;
   }
 
-  addCircle(y, i, step) {
-    let mstep = step / 12;
-    let dot = new createjs.Shape();
-    dot.graphics.beginFill(style.dotBG)
-      .setStrokeStyle(0.5).s(style.textFG).drawCircle(0, 0, 1);
-    dot.x = step * i;
-    dot.y = style.height / 2;
-    let anim = createjs.Tween.get(dot)
-      .to({scaleX: 5, scaleY: 5}, 100, createjs.Ease.cubicOut);
-    let label = new createjs.Text(y, '9pt Sans', style.textFG);
-    label.x = dot.x + 8;
-    label.y = dot.y + 4;
-
-    if (this.data.map[y]) {
-      for (let [i, r] of this.data.map[y].entries()) {
-        console.log(y, r);
-        let m = parseInt(r.startDate.split('-')[1]);
-        let m2 = parseInt(r.endDate.split('-')[1]);
-        let y2 = parseInt(r.endDate.split('-')[0]);
-        let item = new createjs.Shape();
-        item.graphics.beginFill(r.color).drawRect(0, 0, (y2-y)*step + (m2-m)*mstep, (r.studyType ? style.itemHeightBottom : style.itemHeight) + i*style.hStep);
-        item.x = dot.x + (m-1) * mstep;
-        item.y = dot.y - style.itemHeight - 2;
-        if (r.studyType) {
-          item.y = dot.y + 2;
-        }
-        item.scaleX = 0;
-        this.stage.addChild(item);
-        let orig_y = item.y;
-
-        let anim = createjs.Tween.get(item).to({scaleX: 1}, 800, createjs.Ease.cubicOut).call(() => {
-          let l = this.addLabel(item, r);
-          item.on('mouseover', (e) => {
-            let animOver = createjs.Tween.get(item);
-            let animOverL = createjs.Tween.get(l);
-            animOverL.to({scaleX: 1.2, scaleY: 1.2}, 100);
-            if (!r.studyType) {
-              animOver.to({alpha: style.itemAlpha, scaleY: style.itemZoom, y: item.y - style.itemHeight * (style.itemZoom - 1)}, 100);
-            } else {
-              animOver.to({alpha: style.itemAlpha, scaleY: style.itemZoom}, 100);
-            }
-          });
-          item.on('mouseout', (e) => {
-            let animOut = createjs.Tween.get(item);
-            let animOverL = createjs.Tween.get(l);
-            animOverL.to({scaleX: 1, scaleY: 1}, 100);
-            animOut.to({alpha: 1, scaleY: 1, y: orig_y}, 100);
-          });
-        });
-      }
-    }
-    dots.push(dot);
-    let g = new createjs.Container();
-    g.addChild(dot);
-    g.addChild(label);
-    let ha = new createjs.Shape();
-    ha.graphics.beginFill("#000").drawRect(dot.x - 3, dot.y - 3, 20 + label.getMeasuredWidth(), 30);
-    g.hitArea = ha;
-    this.stage.addChild(g);
-    let orig_x = label.x;
-    g.on('mouseover', (e) => {
-      let animOver = createjs.Tween.get(dot);
-      animOver.to({scaleX: 7, scaleY: 7}, 100);
-      let animOverL = createjs.Tween.get(label);
-      animOverL.to({rotation: 30, x: orig_x + 5}, 100);
-    });
-    g.on('mouseout', (e) => {
-      let animOut = createjs.Tween.get(dot);
-      animOut.to({scaleX: 5, scaleY: 5}, 100);
-      let animOutL = createjs.Tween.get(label);
-      animOutL.to({rotation: 0, x: orig_x}, 100);
-    });
-  }
-
-  addLabel(item, r) {
-    let label;
-    let angle = 0;
-    if (!r.studyType) {
-      label = new createjs.Text(r.company, '9pt Sans', style.textFG);
-      label.x = item.x + 30;
-      label.y = item.y - 20;
-      angle = -45;
-    } else {
-      label = new createjs.Text(r.institution, '9pt Sans', style.textFG);
-      label.x = item.x + 30;
-      label.y = item.y + style.itemHeightBottom + 16;
-    }
-
-    label.alpha = 0;
-    label.rotation = 0;
-    let anim = createjs.Tween.get(label).to({alpha: 1, rotation: angle}, 400, createjs.Ease.cubicOut);
-    this.stage.addChild(label);
-    return label;
-  }
 }
 
 function init(event) {
