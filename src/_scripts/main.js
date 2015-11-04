9bab59b5f3a375881538cdadf0c762bb20e0bb32
Averrin <averrin@gmail.com>
2015-11-04 20:57:59 +0300

    router for profile

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,18 +4,29 @@ import $ from 'jquery';
 import director from 'director'
 import style from './style';
 import Timeline from './timeline';
+let queue = new createjs.LoadQueue(true);
+let handler;
 // import Container from 'createjs';
 
 function init(event) {
   let data = event.result;
   window.data = data;
+  let scene = $(`
+    <i class="fa fa-refresh" onclick="reload();" id="reload"></i>
+    <canvas id="timeline"></canvas>
+  `);
+  $('.main-container').html(scene);
   let timeline = new Timeline(data);
+  queue._listeners = {};
+  queue.off(handler);
 }
 
 function init_profile(event) {
   let profile = event.result;
   let content = nunjucks.render('./profile.html', {profile});
   $('.main-container').html(content);
+  queue._listeners = {};
+  queue.off(handler);
 }
 
 window.reload = () => {
@@ -23,14 +34,13 @@ window.reload = () => {
 };
 
 $(() => {
-  let queue = new createjs.LoadQueue(true);
   let router = director.Router({
     timeline: () => {
-      queue.on('fileload', init, this);
+      handler = queue.on('fileload', init, this);
       queue.loadFile('timeline.json');
     },
     profile: () => {
-      queue.on('fileload', init_profile, this);
+      handler = queue.on('fileload', init_profile, this);
       queue.loadFile('profile.json');
     }
   }).init('timeline');
